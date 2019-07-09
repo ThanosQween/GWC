88891a4d3c9ea5d395d8a72253a6ef96fa778d08
ThanosQween <52721631+ThanosQween@users.noreply.github.com>
2019-07-09 15:38:39 -0700

    Add files via upload

---
+++ yara_ting.js
@@ -0,0 +1,35 @@
+
+window.onload = function() {
+  window.setTimeout(fadeout, 7000); //8 seconds
+}
+
+function f1() {
+  document.getElementById('f1').style.opacity = '0';
+}
+function f2() {
+  document.getElementById('f2').style.opacity = '0';
+}
+function f3() {
+  document.getElementById('f3').style.opacity = '0';
+}
+function f4() {
+  document.getElementById('f4').style.opacity = '0';
+}
+function f5() {
+  document.getElementById('f5').style.opacity = '0';
+}
+function f6(){
+  document.getElementById('f6').style.opacity = '0';
+}
+function f7() {
+  document.getElementById('f7').style.opacity = '0';
+}
+function f8() {
+  document.getElementById('f8').style.opacity = '0';
+}
+function f9(){
+  document.getElementById('f9').style.opacity = '0';
+}
+function f10(){
+  document.getElementById('f10').style.opacity = '0';
+}
